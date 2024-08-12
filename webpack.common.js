b2e8dd8df0d5f15c794cde9030b267ac3490d856
kuhvehl <96754228+kuhvehl@users.noreply.github.com>
2024-08-12 16:45:18 -0400

    Setup eslint ignores

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,14 @@
+// eslint-disable-next-line no-undef
 const path = require('path');
+// eslint-disable-next-line no-undef
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+// eslint-disable-next-line no-undef
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
+    // eslint-disable-next-line no-undef
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,9 @@
+// eslint-disable-next-line no-undef
 const { merge } = require('webpack-merge');
+// eslint-disable-next-line no-undef
 const common = require('./webpack.common.js');
 
+// eslint-disable-next-line no-undef
 module.exports = merge(common, {
     mode: 'development',
     devtool: "eval-source-map",
